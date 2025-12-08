e05dd7c0379cd6a10eb81a9c9c64441521dbb776
Elvis <elvisimprsntr+github@gmail.com>
2025-12-08 12:41:02 -0500

    Update ntp_analysis.py

--- ntp_analysis.py
+++ ntp_analysis.py
@@ -28,7 +28,7 @@ Data Source:
 # --- Threshold Parameters ---
 THRESHOLD_OFFSET = 0.010   # 10ms offset compliance
 THRESHOLD_JITTER = 0.005   # 5ms jitter compliance
-THRESHOLD_RTT = 400        # 400ms RTT compliance
+THRESHOLD_RTT = 300        # 300ms RTT compliance
 
 # Load CSV
 df = pd.read_csv("ntp.txt")
